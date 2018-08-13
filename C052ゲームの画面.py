30e0e04a8e186df8a91129e8d1a0cea6322f455d
Ken Nakai <out.of.the.base.sfc@gmail.com>
2018-08-14 00:35:05 +0900

    パイザチャレンジ！

---
+++ C052ゲームの画面.py
@@ -0,0 +1,13 @@
+size = input()
+der = input()
+
+size_split = size.split()
+dr_split = der.split()
+
+height = abs(int(size_split[0]) * int(dr_split[1]))
+width = abs(int(size_split[1]) * int(dr_split[0]))
+zone = abs(int(dr_split[0]) * int(dr_split[1]))
+
+res = height + width - zone
+# print(height, width, zone)
+print(res)
