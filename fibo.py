805258bc0e631207cf9885990fde54f8717a192b
hujuu <out.of.the.base.sfc@gmail.com>
2018-12-10 08:42:21 +0900

    install

---
+++ fibo.py
@@ -0,0 +1,23 @@
+def fib(n):
+    a, b = 0, 1
+
+    while b < n:
+        print(b, end="")
+        a, b = b, a + b
+
+    print()
+
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+
+    return result
+
+if __name__ == "__main__":
+    import sys
+    print(sys.argv[0])
+    fib(int(sys.argv[1]))
