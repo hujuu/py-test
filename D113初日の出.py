e0b432216e0c718db5fa5b8f6391119ca20f9b93
hujuu <out.of.the.base.sfc@gmail.com>
2021-01-18 00:39:03 +0900

    Delete D113初日の出.py

--- D113初日の出.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-time = input()
-
-date_dt = datetime.datetime.strptime(time, '%H:%M')
-
-date_dt = date_dt - datetime.timedelta(hours=8)
-
-print("{}:{}".format(date_dt.hour, date_dt.minute))
